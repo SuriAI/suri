b8848b979a98906a54df04df4cc9485d33af31f1
johnraivenolazo <johnraivenolazo@gmail.com>
2025-08-07 21:48:57 +0800

    Update main window file path to load from dist-react

--- desktop/src/electron/main.ts
+++ desktop/src/electron/main.ts
@@ -3,5 +3,5 @@ import path from "path"
 
 app.on("ready", () => {
 	const mainWindow = new BrowserWindow();
-    mainWindow.loadFile(path.join(app.getAppPath(), "dist/index.html"));
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
 })
