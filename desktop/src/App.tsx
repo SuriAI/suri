dd4efb6ebb3c5ee530c98d301ad3512d84b25377
johnraivenolazo <johnraivenolazo@gmail.com>
2025-09-24 22:21:07 +0800

    Remove old component ref

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,29 +1,12 @@
-import { useState } from 'react'
-import SystemManagement from './components/SystemManagement.tsx'
+
 import LiveVideo from './components/LiveVideo.tsx'
 import TitleBar from './components/TitleBar.tsx'
 
-export type MenuOption = 
-  | 'system-management'
-  | 'live-video'
-  | 'advanced-recognition'
-
 function App() {
-  const [currentMenu, setCurrentMenu] = useState<MenuOption>('live-video')
-
-
 
   const renderCurrentComponent = () => {
-    switch (currentMenu) {
-      case 'system-management':
-        return <SystemManagement onBack={() => setCurrentMenu('live-video')} />
-      case 'live-video':
-      default:
         return <LiveVideo />
-    }
   }
-
-
 
   return (
     <div className="electron-window-container">
