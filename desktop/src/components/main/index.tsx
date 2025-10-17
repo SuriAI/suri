32c249b43d7817b1d78e3c8795e09016322b19ea
johnraivenolazo@gmail.com <johnraivenolazo@gmail.com>
2025-10-18 03:13:45 +0800

    Remove glass hover

--- desktop/src/components/main/index.tsx
+++ desktop/src/components/main/index.tsx
@@ -1859,7 +1859,7 @@ export default function Main() {
         {/* Video Section */}
         <div className="flex-1 flex flex-col min-h-0">
           {/* Video Container */}
-          <div className="relative flex flex-1 min-h-0 items-center justify-center px-4 pt-5">
+          <div className="relative flex flex-1 min-h-0 items-center justify-center px-4 pt-6">
             <VideoCanvas
               videoRef={videoRef}
               canvasRef={canvasRef}
